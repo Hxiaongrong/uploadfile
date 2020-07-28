c7861184e9c47b1c25d8eb9d8eb55091815f204c
houxiangrong <houxiangrong@excean.com>
2020-07-28 17:55:32 +0800

    firstAdd

---
+++ page/src/App.vue
@@ -0,0 +1,180 @@
+<template>
+  <div id="app">
+    <input type="file" @change="handleFileChange">
+    <el-button @click="handleUpload">上传</el-button>
+    <p>计算hash进度</p>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="percentage" />
+    <p>总进度</p>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="uploadPercentage" />
+
+    <el-table :data="data">
+      <el-table-column label="切片hash" prop="hash" />
+      <el-table-column label="大小" prop="size" />
+      <el-table-column label="进度">
+        <template slot-scope="scope">
+          <el-progress :text-inside="true" :stroke-width="26" :percentage="scope.row.percentage" />
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import SparkMD5 from 'spark-md5'
+const SIZE = 10 * 1024 * 1024 // 切片大小
+export default {
+  data: () => ({
+    container: {
+      file: null,
+      hash: ''
+    },
+    data: [],
+    percentage: 0
+  }),
+  computed: {
+    uploadPercentage() {
+      if (!this.container.file || !this.data.length) return 0
+      const loaded = this.data
+        .map(item => item.size * item.percentage)
+        .reduce((acc, cur) => acc + cur)
+      return parseInt((loaded / this.container.file.size).toFixed(2))
+    }
+  },
+  methods: {
+    handleFileChange(e) {
+      const [file] = e.target.files
+      if (!file) return
+      this.container.file = file
+    },
+    calculateHash(fileChunkList) {
+      return new Promise(res => {
+        const spark = new SparkMD5.ArrayBuffer()
+        let count = 0
+        const loadNext = index => {
+          const reader = new FileReader()
+          reader.onload = e => {
+            count++
+            spark.append(e.target.result)
+            if (count === fileChunkList.length) {
+              res(spark.end())
+              this.percentage = 100
+            } else {
+              this.percentage += 100 / fileChunkList.length
+              loadNext(count)
+            }
+          }
+          reader.readAsArrayBuffer(fileChunkList[index].file)
+        }
+        loadNext(0)
+      })
+    },
+    createProgressHandler(item) {
+      return e => {
+        item.percentage = parseInt(String((e.loaded / e.total) * 100))
+      }
+    },
+    createFileChunk(file, size = SIZE) {
+      const chunks = []
+      let cur = 0
+      while (cur < file.size) {
+        chunks.push({ file: file.slice(cur, cur + size) })
+        cur += size
+      }
+      return chunks
+    },
+    async uploadChunks() {
+      const requestList = this.data
+        .map(({ chunk, hash, index, fileHash }) => {
+          const formData = new FormData()
+          formData.append('chunk', chunk)
+          formData.append('hash', fileHash)
+          formData.append('filename', hash)
+          return { formData, index }
+        })
+        .map(({ formData, index }) => this.request({
+          url: 'http://localhost:3000/upload',
+          data: formData,
+          onProgress: this.createProgressHandler(this.data[index])
+        })
+        )
+      const res = await Promise.all(requestList)
+      console.log(res)
+      await this.mergeRequest()
+    },
+    async verifyUpload(filename, fileHash) {
+      const { data } = await this.request({
+        url: 'http://localhost:3000/verify',
+        headers: {
+          'content-type': 'application/json'
+        },
+        data: JSON.stringify({
+          filename, fileHash
+        })
+      })
+      return JSON.parse(data)
+    },
+    async handleUpload() {
+      if (!this.container.file) return
+      const fileChunkList = this.createFileChunk(this.container.file)
+      this.container.hash = await this.calculateHash(fileChunkList)
+      const { shouldUpload } = await this.verifyUpload(
+        this.container.file.name,
+        this.container.hash
+      )
+      if (!shouldUpload) {
+        this.$message.success('秒传：上传成功')
+        return
+      }
+      this.data = fileChunkList.map(({ file }, index) => ({
+        fileHash: this.container.hash,
+        chunk: file,
+        size: file.size,
+        hash: this.container.file.name + '_' + this.container.hash + '-' + index,
+        percentage: 0,
+        index
+      }))
+      await this.uploadChunks()
+    },
+    async mergeRequest() {
+      await this.request({
+        url: 'http://localhost:3000/merge',
+        headers: {
+          'content-type': 'application/json'
+        },
+        data: JSON.stringify({
+          size: SIZE,
+          filename: this.container.file.name,
+          hash: this.container.hash
+        })
+      })
+    },
+    request({ url, method = 'post', data, headers = {}, onProgress = e => e }) {
+      return new Promise(resolve => {
+        const xhr = new XMLHttpRequest()
+        xhr.open(method, url)
+        xhr.upload.onprogress = onProgress
+        Object.keys(headers).forEach(key =>
+          xhr.setRequestHeader(key, headers[key])
+        )
+        xhr.send(data)
+        xhr.onload = e => {
+          resolve({
+            data: e.target.response
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ server/index.js
@@ -0,0 +1,164 @@
+const http = require("http")
+const path = require("path")
+const fse = require("fs-extra")
+const multiparty = require("multiparty")
+
+let test = 0
+
+function deleteFolderRecursive(url) {
+  var files = [];
+  if (fse.existsSync(url)) {
+      files = fse.readdirSync(url);
+      files.forEach(function (file, index) {
+          var curPath = path.join(url, file);
+          if (fse.statSync(curPath).isDirectory()) { // recurse
+              deleteFolderRecursive(curPath);
+          } else {
+              fse.unlinkSync(curPath);
+          }
+      });
+      fse.rmdirSync(url);
+  } else {
+      console.log("给定的路径不存在，请给出正确的路径");
+  }
+}
+
+const server = http.createServer()
+const UPLOAD_DIR = path.resolve(__dirname, "..", "target")
+if (fse.existsSync(UPLOAD_DIR)) {
+  deleteFolderRecursive(UPLOAD_DIR)
+}
+
+const resolvePost = req => {
+  return new Promise((res, rej) => {
+    let chunk = ''
+    req.on('data', data => {
+      chunk += data
+    })
+    req.on('end', () => {
+      res(JSON.parse(chunk))
+    })
+    req.on('error', e => {
+      rej(e)
+    })
+  })
+}
+
+const mergeFileChunk = async (filePath, filename, size, fileType) => {
+  let isArr = Object.prototype.toString.call(size) === '[object Array]'
+  const chunkPaths = await fse.readdir(filePath)
+  chunkPaths.sort((a, b) => a.slice(a.lastIndexOf('-') + 1, a.length) - b.slice(b.lastIndexOf('-') + 1, b.length))
+  await Promise.all(
+    chunkPaths.map((chunkPath, index) => 
+      pipeStream(
+        path.resolve(filePath, chunkPath),
+        fse.createWriteStream(
+          path.resolve(UPLOAD_DIR, filename + '.' + fileType),
+          {
+            start: isArr ? size[index] : index * size,
+            end: isArr ? size[index + 1] : (index + 1) * size
+          }  
+        )
+      )
+    )
+  )
+}
+
+const pipeStream = (path, writeStream) => {
+  return new Promise(res => {
+    const readStream = fse.createReadStream(path)
+    readStream.on('end', () => {
+      res()
+    })
+    readStream.pipe(writeStream)
+  })
+}
+
+const extractExt = filename => {
+  return filename.slice(filename.lastIndexOf("."), filename.length)
+}
+
+const createUploadedList = async fileHash => {
+  return fse.existsSync(path.resolve(UPLOAD_DIR, fileHash)) ? await fse.readdir(path.resolve(UPLOAD_DIR, fileHash)) : []
+}
+
+server.on("request", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Headers", "*")
+  if (req.method === "OPTIONS") {
+    res.status = 200
+    res.end()
+    return
+  }
+  
+  if (req.url === '/upload') {
+    const multipart = new multiparty.Form()
+    // 拿不到解析的fields 和 files，待解决
+    multipart.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      let chunk = ''
+      let hash = ''
+      let filename = ''
+      let chunkDir = ''
+      multipart.on('file', (name ,val) => {
+        if (name === 'chunk') {
+          chunk = val
+        }
+      })
+      multipart.on('field', async (name ,val) => {
+        if (name === 'hash') {
+          hash = val
+          chunkDir = path.resolve(UPLOAD_DIR, hash)
+          if (!fse.existsSync(chunkDir)) { // 是否存在目录
+            await fse.mkdirs(chunkDir)
+          }
+        }
+        if (name === 'filename') {
+          filename = val
+          await fse.move(chunk.path, `${chunkDir}/${filename}`)
+          res.end(JSON.stringify({
+            code: 0,
+            message: "received file chunk"
+          }))
+        }
+      })
+    })
+  }
+
+  if (req.url === '/merge') {
+    const { filename:fileFullname, hash, size } = await resolvePost(req)
+    const fileType = fileFullname.slice(fileFullname.lastIndexOf('.') + 1, fileFullname.length)
+    const filePath = path.resolve(UPLOAD_DIR, hash)
+    mergeFileChunk(filePath, hash, size, fileType)
+    res.end(JSON.stringify({
+      code: 0,
+      message: "file merged success"
+    }))
+  }
+  
+  if (req.url === '/verify') {
+    const data = await resolvePost(req)
+    const { fileHash, filename } = data
+    const ext = extractExt(filename)
+    const filePath = path.resolve(UPLOAD_DIR, fileHash+ext)
+    if (fse.existsSync(filePath)) {
+      res.end(
+        JSON.stringify({ shouldUpload: false })
+      )
+    } else {
+      res.end(
+        JSON.stringify({ 
+          shouldUpload: true,
+          uploadedList: await createUploadedList(fileHash)
+        })
+      )
+    }
+  }
+})
+
+
+
+server.listen(3000, () => console.log("正在监听 3000 端口"))
