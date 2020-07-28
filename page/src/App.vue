1886834fca7b7840c63f98b378c891af70ec58ed
houxiangrong <houxiangrong@excean.com>
2020-07-28 18:41:53 +0800

    并发处理

--- page/src/App.vue
+++ page/src/App.vue
@@ -68,6 +68,67 @@ export default {
         loadNext(0)
       })
     },
+    async calculateHashIdle(chunks) {
+      return new Promise(res => {
+        const spark = new SparkMD5.ArrayBuffer()
+        let count = 0
+        const appendToSpark = async file => {
+          return new Promise(resolve => {
+            const reader = new FileReader()
+            reader.onload = e => {
+              spark.append(e.target.result)
+              resolve()
+            }
+            reader.readAsArrayBuffer(file)
+          })
+        }
+        const workLoop = async deadline => {
+          while (count < chunks.length && deadline.timeRemaining() > 1) {
+            await appendToSpark(chunks[count].file)
+            count++
+            if (count < chunks.length) {
+              this.percentage = Number(((100 * count) / chunks.length).toFixed(2))
+            } else {
+              this.percentage = 100
+              res(spark.end())
+            }
+          }
+          window.requestIdleCallback(workLoop)
+        }
+        window.requestIdleCallback(workLoop)
+      })
+    },
+    async calculateHashSample() {
+      return new Promise(res => {
+        const spark = new SparkMD5.ArrayBuffer()
+        const reader = new FileReader()
+        const file = this.container.file
+        const size = this.container.file.size
+        const offset = 2 * 1024 * 1024
+        const chunks = [file.slice(0, offset)]
+
+        let cur = offset
+        while (cur < size) {
+          this.percentage = parseFloat(cur / size * 100).toFixed(2)
+          if (cur + offset >= size) {
+            chunks.push(file.slice(cur, cur + offset))
+          } else {
+            const mid = cur + offset / 2
+            const end = cur + offset
+            chunks.push(file.slice(cur, cur + 2))
+            chunks.push(file.slice(mid, mid + 2))
+            chunks.push(file.slice(end - 2, end))
+          }
+          cur += offset
+        }
+        reader.onload = e => {
+          spark.append(e.target.result)
+          this.percentage = 100
+          res(spark.end())
+        }
+        reader.readAsArrayBuffer(new Blob(chunks))
+      })
+    },
     createProgressHandler(item) {
       return e => {
         item.percentage = parseInt(String((e.loaded / e.total) * 100))
@@ -82,41 +143,12 @@ export default {
       }
       return chunks
     },
-    async uploadChunks() {
-      const requestList = this.data
-        .map(({ chunk, hash, index, fileHash }) => {
-          const formData = new FormData()
-          formData.append('chunk', chunk)
-          formData.append('hash', fileHash)
-          formData.append('filename', hash)
-          return { formData, index }
-        })
-        .map(({ formData, index }) => this.request({
-          url: 'http://localhost:3000/upload',
-          data: formData,
-          onProgress: this.createProgressHandler(this.data[index])
-        })
-        )
-      const res = await Promise.all(requestList)
-      console.log(res)
-      await this.mergeRequest()
-    },
-    async verifyUpload(filename, fileHash) {
-      const { data } = await this.request({
-        url: 'http://localhost:3000/verify',
-        headers: {
-          'content-type': 'application/json'
-        },
-        data: JSON.stringify({
-          filename, fileHash
-        })
-      })
-      return JSON.parse(data)
-    },
     async handleUpload() {
       if (!this.container.file) return
       const fileChunkList = this.createFileChunk(this.container.file)
-      this.container.hash = await this.calculateHash(fileChunkList)
+      // this.container.hash = await this.calculateHash(fileChunkList)
+      // this.container.hash = await this.calculateHashIdle(fileChunkList) // 利用帧之间的时间切片空闲时间
+      this.container.hash = await this.calculateHashSample(fileChunkList) // 抽样切片
       const { shouldUpload } = await this.verifyUpload(
         this.container.file.name,
         this.container.hash
@@ -134,6 +166,68 @@ export default {
         index
       }))
       await this.uploadChunks()
+    },
+    async verifyUpload(filename, fileHash) {
+      const { data } = await this.request({
+        url: 'http://localhost:3000/verify',
+        headers: {
+          'content-type': 'application/json'
+        },
+        data: JSON.stringify({
+          filename, fileHash
+        })
+      })
+      return JSON.parse(data)
+    },
+    async uploadChunks() {
+      const requestList = this.data
+        .map(({ chunk, hash, index, fileHash }) => {
+          const formData = new FormData()
+          formData.append('chunk', chunk)
+          formData.append('hash', fileHash)
+          formData.append('filename', hash)
+          return { formData, index }
+        })
+        // 不能很好的处理并发
+        // .map(({ formData, index }) => this.request({
+        //   url: 'http://localhost:3000/upload',
+        //   data: formData,
+        //   onProgress: this.createProgressHandler(this.data[index])
+        // }))
+      // const res = await Promise.all(requestList)
+      const res = await this.sendRequest(requestList, 4)
+      console.log(res)
+      await this.mergeRequest()
+    },
+    async sendRequest(forms, max = 4) {
+      return new Promise(res => {
+        const len = forms.length
+        let idx = 0
+        let counter = 0
+        const start = async() => {
+          while (idx < len && max > 0) {
+            max--
+            const form = forms[idx].formData
+            const index = forms[idx].index
+            idx++
+            this.request({
+              url: 'http://localhost:3000/upload',
+              data: form,
+              onProgress: this.createProgresshandler(this.chunks[index]),
+              requestList: this.requestList
+            }).then(() => {
+              max++
+              counter++
+              if (counter === len) {
+                res()
+              } else {
+                start()
+              }
+            })
+          }
+        }
+        start()
+      })
     },
     async mergeRequest() {
       await this.request({
